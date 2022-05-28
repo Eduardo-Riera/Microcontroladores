1bf40d855bca596a92276ab625590d10e2eb7774
Eduardo Riera <72780130+Eduardo-Riera@users.noreply.github.com>
2022-05-28 17:23:55 -0300

    Add files via upload

---
+++ Enciende-y-apaga-un-LED-8515.cpp
@@ -0,0 +1,14 @@
+
+int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);  
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
